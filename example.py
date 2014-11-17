975261730f9be9afd8f21856ce48fe489bdb461a
Sorch <support@sorch.info>
2014-11-17 18:20:56 +0000

    added a _init thing so you can set your lovely font colours

--- example.py
+++ example.py
@@ -4,8 +4,13 @@ import conf
 
 class Bot(coco.internals.Manager):
 
+	def _init(self):
+		self.setNameColor("#ff0")
+		self.setFontColor("#3bf")
+		self.run()
+
 	def _on_Message(self, group, user, post):
 		ret = "%s: %s: %s" % (group.name, user, post)
 		print(ret)
 
-bot = Bot(conf.groups, conf.name, conf.password).run()
+bot = Bot(conf.groups, conf.name, conf.password)._init()
