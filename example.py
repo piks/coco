199ea07457e4eb9fcce2539e9e2140f720a46d20
piks <piks@users.noreply.github.com>
2014-11-17 17:04:53 -0500

    fix small glitch

--- example.py
+++ example.py
@@ -3,10 +3,11 @@ import conf
 
 
 class Bot(coco.internals.Manager):
-
+	def _p_ok(self, group):
+		group.setNameColor("ff0")
+		group.setFontColor("3bf")
+		print("Connected to "+group.name)
 	def _init(self):
-	#	self.setNameColor("#ff0")
-	#	self.setFontColor("#3bf")
 		self.run()
 
 	def _on_Message(self, group, user, msg):
